b0d6e45e04a8e58a7406050da634cf3b915e4c4e
Luke Hudson <lukeletters@gmail.com>
2014-08-26 19:11:16 +0200

    Upgrade modules and move to Express 4.x

--- app/config/express.js
+++ app/config/express.js
@@ -1,17 +1,24 @@
 var express = require('express');
 var session = require('express-session');
 
+var bodyParser     = require('body-parser');
+var express        = require('express');
+var favicon        = require('serve-favicon');
+var logger         = require('morgan');
+var methodOverride = require('method-override');
+var serveStatic    = require('serve-static');
+var session        = require('express-session');
+var path           = require('path');
+
 module.exports = function(app, config) {
-  app.configure(function () {
-    app.use(express.compress());
-    app.use(express.static(config.root + '/public'));
+    app.use(serveStatic(config.root + '/public'));
     app.set('port', config.port);
     app.set('views', config.root + '/app/views');
     app.set('view engine', 'jade');
-    app.use(express.favicon(config.root + '/public/img/favicon.ico'));
-    app.use(express.logger('dev'));
-    app.use(express.bodyParser());
-    app.use(express.methodOverride());
+    app.use(favicon(config.root + '/public/img/favicon.png'));
+    app.use(logger('dev'));
+    app.use(bodyParser());
+    app.use(methodOverride());
     app.use(session({
         secret: 'JJDd8sjHd8345J4Ns33lsKJj',
         resave: true,
@@ -19,10 +26,4 @@ module.exports = function(app, config) {
         // secure: true // NEEDS HTTPS
     }));
 
-
-    app.use(app.router);
-    app.use(function(req, res) {
-      res.status(404).render('404', { title: '404' });
-    });
-  });
 };
